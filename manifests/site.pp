4d2bbba0213b4ad7b5265e37fe04bf3d57469c4a
stormbull <jhschmidt@outlook.com>
2016-09-27 14:10:16 -0400

    lab

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,12 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+  
+  file {'/etc/motd':
+    ensure => file,
+    owner => 'root'
+    group => 'root'
+    mode => '0644'
+    content => "today i learned what it means to manage state using Puppet.\n"
+    }
 }
