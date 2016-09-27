7b0ad9e96f7a0dc591ae9363f064b20e310dbc2f
stormbull <jhschmidt@outlook.com>
2016-09-27 15:28:15 -0400

    Update init.pp
    9.3 add skeleton class

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,3 +7,18 @@ ensure => present,
 include users
 }
 
+class skeleton {
+file { '/etc/skel':
+ensure => directory,
+owner => 'root',
+group => 'root',
+mode => '0755',
+}
+file { '/etc/skel/.bashrc':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0644',
+source => 'puppet:///modules/skeleton/bashrc',
+}
+}
