a8e28988d56ca222aece27a9e4d2ce58786e3880
stormbull <jhschmidt@outlook.com>
2016-09-28 14:19:48 -0400

    17.1: Hiera Functions
    $message = hiera('message')
    notify { $message: }

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,6 +44,9 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
   
+  $message = hiera('message')
+notify { $message: }
+  
   if $::virtual != 'physical' {
 $vmname = capitalize($::virtual)
 notify { "This is a ${vmname} virtual machine.": }
