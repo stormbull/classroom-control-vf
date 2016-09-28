7854ba51fc3a77f0fa40fededfcf382359c1735e
stormbull <jhschmidt@outlook.com>
2016-09-28 16:13:00 -0400

    18.1: Parameterized Classes

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,33 +1,28 @@
-class nginx {
-File {
-owner => 'root',
-group => 'root',
-mode => '0664',
+class nginx (
+$root = undef,
+) {
+case $::osfamily {
+'redhat','debian' : {
+$package = 'nginx'
+$owner = 'root'
+$group = 'root'
+# $docroot = '/var/www'
+$confdir = '/etc/nginx'
+$logdir = '/var/log/nginx'
+# this will be used if we don't pass in a value
+$default_docroot = '/var/www'
 }
-package { 'nginx':
-ensure => present,
+'windows' : {
+$package = 'nginx-service'
+$owner = 'Administrator'
+$group = 'Administrators'
+# $docroot = 'C:/ProgramData/nginx/html'
+$confdir = 'C:/ProgramData/nginx'
+$logdir = 'C:/ProgramData/nginx/logs'
+# this will be used if we don't pass in a value
+$default_docroot = 'C:/ProgramData/nginx/html'
 }
-file { [ '/var/www', '/etc/nginx/conf.d' ]:
-ensure => directory,
-}
-file { '/var/www/index.html':
-ensure => file,
-source => 'puppet:///modules/nginx/index.html',
-}
-file { '/etc/nginx/nginx.conf':
-ensure => file,
-source => 'puppet:///modules/nginx/nginx.conf',
-require => Package['nginx'],
-notify => Service['nginx'],
-}
-file { '/etc/nginx/conf.d/default.conf':
-ensure => file,
-source => 'puppet:///modules/nginx/default.conf',
-notify => Service['nginx'],
-require => Package['nginx'],
-}
-service { 'nginx':
-ensure => running,
-enable => true,
+default : {
+fail("Module ${module_name} is not supported on ${::osfamily}")
 }
 }
